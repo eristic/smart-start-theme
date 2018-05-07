5a822a28c9e24182f1a3241f9e839139c0c6bba9
Scott Stewart <scott.c.stewart@gmail.com>
2018-05-07 16:24:31 -0700

    Adds functionality to single blog post.

--- single-post.php
+++ single-post.php
@@ -2,7 +2,7 @@
 
 	<header class="page-header">
 	
-		<h1 class="page-title">Welcome to our blog</h1>
+		<h1 class="page-title"><?php the_title();?></h1>
 		
 	</header><!-- end .page-header -->
 
@@ -10,155 +10,79 @@
 
 		<article class="entry single clearfix">
 
-			<a href="single-post.html" title="">
-				<img src="img/placeholders/blog-post-1.jpg" alt="" class="entry-image">
+			<a href="<?php the_permalink();?>" title="">
+				<?php the_post_thumbnail('blog-post', array('class' => 'entry-image'));?>
 			</a>
 
 			<div class="entry-body">
 
-				<a href="#">
-					<h1 class="title">Bringing iMessage to the Desktop</h1>
+				<a href="<?php the_permalink();?>">
+					<h1 class="title"><?php the_title();?></h1>
 				</a>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet lorem lacinia et. Cras et ligula libero. Quisque quis magna vitae ipsum consequat varius in ut ante. Maecenas a mi nibh, eu euismod orci. Vivamus viverra lacus vitae tortor molestie malesuada. In nulla nibh, congue sed molestie nec, dapibus sed purus.</p>
-
-				<p>Maecenas pulvinar blandit facilisis. Curabitur mollis nisl non purus ultrices convallis. Ut volutpat tristique nisl elementum ultricies. Etiam vitae neque leo. Donec eget turpis lorem. Nulla facilisi. Duis bibendum diam sed ligula euismod a interdum ipsum semper. Curabitur id lectus pulvinar erat pretium aliquet. Ut felis enim, congue ac ullamcorper id, luctus congue neque. Praesent augue mauris, lacinia quis auctor nec, vestibulum suscipit ligula. Nunc elit nisl, mollis ut consequat a, sodales eu tortor. Aenean egestas, nisi a iaculis lacinia, mauris dui accumsan dui, vel scelerisque neque nibh sed nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-
-				<blockquote>Praesent bibendum lobortis lectus, quis dictum risus faucibus sagittis. Mauris a placerat lacus. Mauris rhoncus dolor sit amet nisl volutpat at consequat tortor feugiat. Ut ornare dui eu ipsum lobortis rhoncus.</blockquote>
-
-				<p>Quisque odio urna, ultrices non volutpat lacinia, scelerisque sit amet justo. Quisque venenatis sapien id eros pulvinar sit amet posuere lacus gravida. Aenean accumsan placerat nulla, dictum semper turpis scelerisque sit amet. Cras et quam lorem. Fusce rhoncus consequat nulla ut gravida. Sed adipiscing, lacus ac commodo scelerisque, est erat eleifend augue, nec viverra ligula lacus sed arcu. Suspendisse eu massa quis tellus dapibus dignissim suscipit eget est. Sed ullamcorper elit a arcu consequat in pellentesque odio volutpat. Donec dignissim porttitor consectetur. Donec risus mauris, aliquet ut ullamcorper eu, dignissim ut nulla. Pellentesque in magna eros, eget viverra nulla. In dui eros, porttitor a vehicula et, vulputate hendrerit urna. Mauris nec nibh turpis.<p>
-
-				<ul>
-					<li>Curabitur id lectus pulvinar</li>
-					<li>Ut felis enim, congue</li>
-					<li>Praesent augue mauris</li>
-					<li>Nunc elit nisl mollis</li>
-				</ul>
-
-				<p>Etiam auctor tincidunt augue at pharetra. Morbi velit eros, sagittis in facilisis non, rhoncus et erat. Nam posuere tristique sem, eu ultricies tortor imperdiet vitae. Curabitur lacinia neque non metus scelerisque tincidunt. Maecenas accumsan imperdiet faucibus. Mauris tincidunt, nulla quis rhoncus malesuada, nibh ante pulvinar dolor, ut lacinia libero risus nec orci.</p>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; endif; ?>
 				
 			</div><!-- end .entry-body -->
 
 			<div class="entry-meta">
 
-				<ul>
-					<li><a href="single-post.html"><span class="post-format">Permalink</span></a></li>
-					<li><span class="title">Posted:</span> <a href="#">Sep 17 2011</a></li>
-					<li><span class="title">Tags:</span> <a href="#">Standard</a></li>
-					<li><span class="title">Comments:</span> <a href="#">3</a></li>
-				</ul>
+			<ul>
+				<li><a href="<?php the_permalink();?>"><span class="post-format ">Permalink</span></a></li>
+				<li><span class="title">Posted:</span> <a href="#"><?php the_date('M d Y');?></a></li>
+				<li><span class="title">Tags:</span> 
+				<?php $tags = get_the_tags(); 
+				if( $tags ) foreach( $tags as $tag ) { ?>
+				<a href="<?php echo get_tag_link($tag->term_id); ?>"><?php echo $tag->name; ?></a>
+				<?php } else {
+					// Nothing
+				} ?>
+				
+				<li><span class="title">Comments:</span> <a href="<?php comments_link();?>"><?php comments_number();?></a></li>
+			</ul>		
 
-			</div><!-- end .entry-meta -->
+	</div><!-- end .entry-meta -->
 			
 		</article><!-- end .entry -->
 
 		<section id="comments">
 
-			<h6 class="section-title">Comments (4)</h6>
+			<h6 class="section-title"><?php comments_number();?></h6>
 
-			<ol class="comments-list">
+		
 
-				<li class="comment">
+					<ol class="comment-list">
 
-					<article>
+						<?php
 
-						<img src="http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&amp;s=50" alt="Image" class="avatar">
+						// The Query
+						$comments_query = new WP_Comment_Query;
+						$comments = $comments_query->query($arg);
 
-						<div class="comment-meta">
+						// Comment Loop
+						foreach ( $comments as $comment ) {
+							echo '<li class="comment">';
+							echo '<article>';
+							echo get_avatar( $comment, 50 );
+							echo '<div class=""comment-meta">';
+							echo '<h5 class="author"';
+								echo '<a href="' . $comment->comment_url . '">' . $comment->comment_author . '</a>';
+								echo ' - ';
+								comment_reply_link();
+							echo '</h5>';
 
-							<h5 class="author"><a href="#">John Doe</a> - <a href="#" class="comment-reply-link">Reply</a></h5>
+							echo '<p class="date">' . comment_date('F d, Y') . '</p>';
+								'</div>';
+							echo '<p>' . $comment->comment_content . '</p>';
+							echo '</article>';
+							echo '</li>';
+						} 
+						
+						?>
+						 
+					</ol><!-- .comment-list -->
 
-							<p class="date">January 06, 2011</p>
-							
-						</div><!-- end .comment-meta -->
-
-						<div class="comment-body">
-
-							<p>Morbi velit eros, sagittis in facilisis non, rhoncus et erat. Nam posuere tristique sem, eu ultricies tortor imperdiet vitae. Curabitur lacinia neque non metus.</p>
-							
-						</div><!-- end .comment-body -->
-
-					</article>
-
-					<ul class="children">
-
-						<li class="comment">
-
-							<article>
-
-								<img src="http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&amp;s=50" alt="Image" class="avatar">
-
-								<div class="comment-meta">
-
-									<h5 class="author"><a href="#">John Doe</a> - <a href="#" class="comment-reply-link">Reply</a></h5>
-
-									<p class="date">January 06, 2011</p>
-									
-								</div><!-- end .comment-meta -->
-
-								<div class="comment-body">
-
-									<p>Morbi velit eros, sagittis in facilisis non, rhoncus et erat. Nam posuere tristique sem, eu ultricies tortor imperdiet vitae. Curabitur lacinia neque non metus.</p>
-									
-								</div><!-- end .comment-body -->
-
-							</article>
-
-						</li>
-
-					</ul><!-- end .children -->
-
-				</li>
-
-				<li class="comment">
-
-					<article>
-
-						<img src="http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&amp;s=50" alt="Image" class="avatar">
-
-						<div class="comment-meta">
-
-							<h5 class="author"><a href="#">John Doe</a> - <a href="#" class="comment-reply-link">Reply</a></h5>
-
-							<p class="date">January 06, 2011</p>
-							
-						</div><!-- end .comment-meta -->
-
-						<div class="comment-body">
-
-							<p>Morbi velit eros, sagittis in facilisis non, rhoncus et erat. Nam posuere tristique sem, eu ultricies tortor imperdiet vitae. Curabitur lacinia neque non metus.</p>
-							
-						</div><!-- end .comment-body -->
-
-					</article>
-
-				</li>
-
-				<li class="comment">
-
-					<article>
-
-						<img src="http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&amp;s=50" alt="Image" class="avatar">
-
-						<div class="comment-meta">
-
-							<h5 class="author"><a href="#">John Doe</a> - <a href="#" class="comment-reply-link">Reply</a></h5>
-
-							<p class="date">January 06, 2011</p>
-							
-						</div><!-- end .comment-meta -->
-
-						<div class="comment-body">
-
-							<p>Morbi velit eros, sagittis in facilisis non, rhoncus et erat. Nam posuere tristique sem, eu ultricies tortor imperdiet vitae. Curabitur lacinia neque non metus.</p>
-							
-						</div><!-- end .comment-body -->
-
-					</article>
-
-				</li>
-
-			</ol>
+				<?php comment_form(); ?>
 			
 		</section>
 
